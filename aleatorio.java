d4724548eb22cc35e60d3eb98c16ddad0a16c39d
Jair Aoki <jair2672@gmail.com>
2026-07-31 20:44:19 -0300

    Deixando o numero fixo

--- aleatorio.java
+++ aleatorio.java
@@ -1,25 +1,27 @@
-import java.util.Scanner;
 import java.util.Random;
+import java.util.Scanner;
 
 public class aleatorio{
     public static void main(String[] Args){
         Scanner teclado= new Scanner(System.in);
         Random gerarnumero = new Random();
-        int numero=0, n_random=9;
-        
-        while(numero!=n_random){
-            n_random=gerarnumero.nextInt(0,10);
-            
-            System.out.print("Digite um numero: ");
-            
-            numero = teclado.nextInt();
+        int numero=0, n_random=0;
 
-            System.out.printf("O numero do computador foi %d e o seu foi %d \n", n_random, numero);
+        n_random=gerarnumero.nextInt(0,10);
+        
+        System.out.print("Digite um numero: ");
+
+        numero=teclado.nextInt();
+        
+        while(numero != n_random){
             
-            if (numero==n_random){
-                System.out.println("Parabéns, você acertou o numero");
-            }
+            System.out.print("Tente outro numero: ");
+
+            numero=teclado.nextInt();
         }
+
+        System.out.println("Parabéns, você acertou!!!");
+        
         teclado.close();
     }
 }
