6be80f54cdf2b10e33691bf807064ee39fd3a3ab
Jair Aoki <jair2672@gmail.com>
2026-08-05 12:27:21 -0300

    Atividade OO

--- aleatorio.java
+++ aleatorio.java
@@ -5,28 +5,31 @@ public class aleatorio{
     public static void main(String[] Args){
         Scanner teclado= new Scanner(System.in);
         Random gerarnumero = new Random();
-        int numero = 999, n_random = 999;
+        int numero = 999, n_random = 999, tentativas=0;
 
         n_random = gerarnumero.nextInt(0,10);
         
         System.out.print("Digite um numero entre 0 e 9: ");
         
         while(numero != n_random){
+            tentativas++;
 
             while(!teclado.hasNextInt()){
-                System.out.println("Use apenas inteiros");
+                System.out.print("Use apenas inteiros: ");
                 teclado.next();
-                System.out.print("Tente outro numero: ");
             }
 
             numero = teclado.nextInt();
 
-            if (numero != n_random){
-                System.out.print("Tente outro numero: ");
+            if (numero > n_random){
+                System.out.print("Palpite menor: ");
+            }
+            
+            else if(numero < n_random){
+                System.out.print("Palpite maior:  ");
             }
         }
-
-        System.out.println("Parabéns, você acertou!!!");
+        System.out.printf("Acertou em %d tentativas", tentativas);
         
         teclado.close();
     }
